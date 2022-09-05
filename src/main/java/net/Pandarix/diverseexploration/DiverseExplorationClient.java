3e416e9acf0e876983578338d70df1397ae086e6
Ruven <ruven@uni-bremen.de>
2022-09-05 23:36:18 +0200

    initial commit

---
+++ src/main/java/net/Pandarix/diverseexploration/DiverseExplorationClient.java
@@ -0,0 +1,11 @@
+package net.Pandarix.diverseexploration;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiverseExplorationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}

2b8b8d275d41b278893a3ab77a5c60ca8831d969
Ruven <ruven@uni-bremen.de>
2022-09-05 23:29:13 +0200

    Initial commit

---
+++ src/main/java/net/Pandarix/diverseexploration/DiverseExploration.java
@@ -0,0 +1,14 @@
+package net.Pandarix.diverseexploration;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DiverseExploration implements ModInitializer {
+	public static final String MOD_ID = "diverseexploration";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
